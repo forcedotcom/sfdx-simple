9da1c369e064b648d2fe4cbd5744a6cd07e7e4ff
Chris Wall <cwall@salesforce.com>
2016-10-17 15:27:48 -0500

    Update heroku-create.sh

--- utils/heroku-create.sh
+++ utils/heroku-create.sh
@@ -8,11 +8,8 @@ heroku pipelines:add $pipelineName --app $prodName --stage production
 heroku buildpacks:add https://github.com/heroku/salesforce-buildpack --app $stagingName
 heroku buildpacks:add https://github.com/heroku/salesforce-buildpack --app $prodName
 
-heroku addons:create salesforce:byoo --app $stagingName
-heroku addons:create salesforce:byoo --app $prodName
+heroku addons:create salesforce:standard --app $stagingName
+heroku addons:create salesforce:standard --app $prodName
 
 heroku config:set HEROKU_APP_NAME=$stagingName --app $stagingName
 heroku config:set HEROKU_APP_NAME=$prodName --app $prodName
-
-heroku config:set SALESFORCE_BYOO=true --app $stagingName
-heroku config:set SALESFORCE_BYOO=true --app $prodName
